30601c777ee29fb6890138639df169c4f66dbad3
OlgaK1029 <118237828+OlgaK1029@users.noreply.github.com>
2022-12-20 00:37:46 -0500

    added the search engine

--- src/app.js
+++ src/app.js
@@ -34,10 +34,18 @@ function displayTemperature(response) {
     iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+function search(city) {
+    let apiKey = "b6c3a27632b3df2db782c7182823a95a";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+search("New York");
 
-
-let apiKey = "b6c3a27632b3df2db782c7182823a95a";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
 
